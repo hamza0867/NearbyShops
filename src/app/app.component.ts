d14fae4939d85666b2355a894948003fdcaaab33
hamza0867 <hamza0867@gmail.com>
2018-12-30 17:53:57 +0100

    Clean the code more, use of new view for each page, on the way to fully implement the SPA with Agular routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,11 @@
 import { Component, OnInit } from "@angular/core";
-import { ShopService } from "./services/shop.service";
-import { Shop } from "./shop/shop";
 
 @Component({
     selector: "app-root",
     styleUrls: ["./app.component.scss"],
     templateUrl: "./app.component.html"
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
     public title = "NearbyShops";
     public logo = "../assets/ur.png";
-    public shops: Shop[];
-
-    constructor(private shopService: ShopService) {}
-
-    public ngOnInit() {
-        this.shops = this.shopService.shops;
-    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
+import { RouterModule, Routes } from "@angular/router";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { AuthViewComponent } from "./auth-view/auth-view.component";
 import { ShopService } from "./services/shop.service";
 import { ShopComponent } from "./shop/shop.component";
+import { ShopsViewComponent } from "./shops-view/shops-view.component";
+
+const appRoutes: Routes = [
+    { path: "shops", component: ShopsViewComponent },
+    { path: "", component: ShopsViewComponent },
+    { path: "auth", component: AuthViewComponent }
+];
 
 @NgModule({
-    declarations: [AppComponent, ShopComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [
+        AppComponent,
+        ShopComponent,
+        ShopsViewComponent,
+        AuthViewComponent
+    ],
+    imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
     providers: [ShopService],
     bootstrap: [AppComponent]
 })
