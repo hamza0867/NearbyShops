e7b2af3b2a781fd55df0f291517ed48374689c76
hamza0867 <hamza0867@gmail.com>
2018-12-31 17:13:11 +0100

    Add auth guard to the shops page, Add 404 component for non existing pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule, Routes } from "@angular/router";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { AuthViewComponent } from "./auth-view/auth-view.component";
+import { FourOhFourComponent } from "./four-oh-four/four-oh-four.component";
+import { AuthGuard } from "./services/auth-guard.service";
+import { AuthService } from "./services/auth.service";
 import { ShopService } from "./services/shop.service";
 import { ShopComponent } from "./shop/shop.component";
 import { ShopsViewComponent } from "./shops-view/shops-view.component";
-import { AuthService } from "./services/auth.service";
-import { AuthViewComponent } from "./auth-view/auth-view.component";
 
 const appRoutes: Routes = [
-    { path: "shops", component: ShopsViewComponent },
+    { path: "shops", canActivate: [AuthGuard], component: ShopsViewComponent },
+    { path: "auth", component: AuthViewComponent },
     { path: "", component: AuthViewComponent },
-    { path: "auth", component: AuthViewComponent }
+    { path: "not-found", component: FourOhFourComponent },
+    { path: "**", redirectTo: "/not-found" }
 ];
 
 @NgModule({
@@ -21,10 +25,11 @@ const appRoutes: Routes = [
         AppComponent,
         ShopComponent,
         ShopsViewComponent,
-        AuthViewComponent
+        AuthViewComponent,
+        FourOhFourComponent
     ],
     imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
-    providers: [ShopService, AuthService],
+    providers: [ShopService, AuthService, AuthGuard],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
